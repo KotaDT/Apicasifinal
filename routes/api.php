c5b4ace405559ab694132726622bb990b4653239
KotaDT <fabriziol.adam@gmail.com>
2022-06-26 15:57:36 -0400

    api vbeta

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\AlumnosController;
+use App\Http\Controllers\EducadorasController;
+use App\Http\Controllers\EventosController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('/curso', CursoController::class);
+Route::apiResource('alumnos',AlumnosController::class);
+Route::apiResource('/educadoras', EducadorasController::class);
+Route::apiResource('/eventos', EventosController::class);
